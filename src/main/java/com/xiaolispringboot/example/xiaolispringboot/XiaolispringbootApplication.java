f31075d9d4622c7976fe1115abfcb234ec960891
unknown <XiaoliHugnrAndHumbel>
2020-05-19 09:12:15 +0800

    init xiaoli

---
+++ src/main/java/com/xiaolispringboot/example/xiaolispringboot/XiaolispringbootApplication.java
@@ -0,0 +1,13 @@
+package com.xiaolispringboot.example.xiaolispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaolispringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaolispringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaolispringboot/example/xiaolispringboot/XiaolispringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaolispringboot.example.xiaolispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaolispringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
